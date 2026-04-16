e36d40723385e0b37fe65c0c1d87f6aa5943641d
dripston <dripstone024@gmail.com>
2026-04-16 18:36:43 +0530

    initial commit

---
+++ calci.py
@@ -0,0 +1,25 @@
+import maths  
+
+class Calculator:
+    def add(self, x, y):
+        return x + y
+    
+    def subtract(self, x, y):
+        return x - y
+    
+    def multiply(self, x, y)   
+        return x * y
+    
+    def divide(self, x, y):
+        return x / y
+
+
+calc = Calculator()
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+print("Sum:", calc.add(a, b))
+print("Difference:", calc.subtract(a, b))
+print("Product:", calc.multiply(a, b))
+print("Quotient:", calc.divide(a, b)
